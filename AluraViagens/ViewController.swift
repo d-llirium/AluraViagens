b82e4ab247d2fce7c72d3cdf9f0fc814a2a77012
d-llirium <plimacarlos@brq.com>
2022-05-31 16:40:27 -0300

    Initial Commit

---
+++ AluraViagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AluraViagens
+//
+//  Created by user on 31/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
